2c00fd04d9b15e5174ada812b5efcb4ab8a9020a
NunzioBasciano <nbasho1988@gmail.com>
2024-07-23 14:53:18 +0200

    feat: add dynamic book list

--- 22-07-24/myProduct/src/api/productClient.js
+++ 22-07-24/myProduct/src/api/productClient.js
@@ -4,9 +4,25 @@ export const getProductList = () => {
             resolve(() => {
                 return [{
                     id: 1,
-                    name: "Product 1",
+                    name: "Uniq one",
                     image: "#",
-                    description: "Product 1 Description",
+                    description: `TRATTAMENTO PER CAPELLI TUTTO IN UNO
+
+Innovazione mondiale nella cura dei capelli.
+
+La prima maschera spray senza risciacquo disponibile sul mercato che offre ai capelli 10 BENEFICI fondamentali di cui hanno bisogno.`,
+                    barcode: "1234567890",
+                    cost: 3.4,
+                    price: 12.9,
+                    quantity: 12
+                },
+                {
+                    id: 2,
+                    name: "Uniq one green tea",
+                    image: "#",
+                    description: `Revlon Professional UniqONE Green Tea Scent Hair Treatment è un trattamento che racchiude 10 benefici reali in un solo prodotto con una fresca e profumata fragranza al Thé Verde.
+
+Ideale per capelli secchi e danneggiati da fattori ambientali o chimici come trattamento di riparazione della fibra, apportando estrema lucentezza ed eliminando l'effetto crespo per ciocche favolosamente morbide.`,
                     barcode: "1234567890",
                     cost: 3.4,
                     price: 12.9,
