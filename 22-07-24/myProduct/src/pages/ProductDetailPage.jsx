1e7660a169c74eb50da4da32500074367fe92304
NunzioBasciano <nbasho1988@gmail.com>
2024-07-29 21:57:13 +0200

    fix: remove background of detail image

--- 22-07-24/myProduct/src/pages/ProductDetailPage.jsx
+++ 22-07-24/myProduct/src/pages/ProductDetailPage.jsx
@@ -79,7 +79,7 @@ function ProductDetailPage() {
                             <img
                                 alt=""
                                 src={product.image}
-                                className="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
+                                className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
                             />
                         </div>
                         <div className="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
