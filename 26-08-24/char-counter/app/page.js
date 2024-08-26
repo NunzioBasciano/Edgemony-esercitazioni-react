074a439ac7039d0b33b7cec1590739a85126e0c9
NunzioBasciano <nbasho1988@gmail.com>
2024-08-26 18:42:22 +0200

    feat: handleInputChange, handleCheckboxChange, charCounter

---
+++ 26-08-24/char-counter/app/page.js
@@ -0,0 +1,43 @@
+"use client";
+
+import { useState } from 'react';
+
+export default function Home() {
+  const [input, setInput] = useState('');
+  const [excludeSpaces, setExcludeSpaces] = useState(false);
+
+  const handleInputChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const handleCheckboxChange = (event) => {
+    setExcludeSpaces(event.target.checked);
+  };
+
+  const charCounter = () => {
+    return excludeSpaces ? input.replace(/\s+/g, '').length : input.length;
+  };
+
+  return (
+    <div>
+      <h1>Conta Caratteri</h1>
+      <textarea
+        value={input}
+        onChange={handleInputChange}
+        rows="4"
+        cols="50"
+      ></textarea>
+      <br />
+      <label>
+        <input
+          type="checkbox"
+          checked={excludeSpaces}
+          onChange={handleCheckboxChange}
+        />
+        Escludi gli spazi
+      </label>
+      <br />
+      <p>Numero di caratteri{excludeSpaces ? ' (escludendo gli spazi)' : ''}: {charCounter()}</p>
+    </div>
+  );
+}
